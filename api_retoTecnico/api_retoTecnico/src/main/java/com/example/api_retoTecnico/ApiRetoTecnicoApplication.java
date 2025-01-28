00d15811ac2e034e22fb10c617472aa2fd6ef1aa
Roosevelt08 <alexraod21@gmail.com>
2025-01-28 00:38:01 -0500

    first commit

---
+++ api_retoTecnico/api_retoTecnico/src/main/java/com/example/api_retoTecnico/ApiRetoTecnicoApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_retoTecnico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRetoTecnicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRetoTecnicoApplication.class, args);
+	}
+
+}
